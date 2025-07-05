ce1c87d10a19008a57e9a51c9b78eeaee38ccad4
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-07-05 18:54:48 +0530

    complete creting nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.app')
+
+@section('content')
+    <section class="hero">
+        <h1>Welcome to Anuranga24</h1>
+        <p>We provide vehicles and drivers for site work with full tracking.</p>
+        <a href="/projects">View Projects</a>
+    </section>
+@endsection
