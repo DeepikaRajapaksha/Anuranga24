77ed88eeb89b1d23745c380f4f6b1ec1a3ee9a3d
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-07-05 20:53:07 +0530

    add home page to image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
     <section class="hero">
-        <h1>Welcome to Anuranga24</h1>
-        <p>We provide vehicles and drivers for site work with full tracking.</p>
-        <a href="/projects">View Projects</a>
+        @include('partials.nav')
+        <div class="video-wrapper">
+            <video autoplay muted loop playsinline>
+                <source src="{{ asset('videos/jcb_intro.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+        <div class="hero-text">
+            <h1>Welcome to Anuranga24</h1>
+        </div>
     </section>
 @endsection
