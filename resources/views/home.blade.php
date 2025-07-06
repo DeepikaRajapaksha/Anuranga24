9e4412fbd0cd947778e6a8c29f468cf683b2bbde
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-07-06 11:46:03 +0530

    Add section part

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,4 +13,15 @@
             <h1>Welcome to Anuranga24</h1>
         </div>
     </section>
+    <section>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+    </section>
+    <section>
+        <h1>Our Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+    </section>
+    <section>
+        <h1>Contact Us</h1>
+    </section>
 @endsection
